0f0e4f71cfba4c5bee6fbea10431d1b781ae3340
otsuboDesi <otsubodesignare@gmail.com>
2024-07-31 17:59:41 +0900

    Add: Propsをコンポーネントに渡す

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,16 @@
+import { ColoredMessage } from "./components/ColoredMessage";
+
 export const App = () => {
   const onClickButton = () => {
     alert();
   };
 
-  //CSSオブジェクト
-  const contentStyle = {
-    color: "blue",
-    fontSize: "20px",
-  };
   return (
     <>
       {console.log("test")}
       <h1 style={{ color: "red" }}>Hello!</h1>
-      <p style={contentStyle}>How are you?</p>
+      <ColoredMessage color="blue" message="How are you?" />
+      <ColoredMessage color="pink" message="I'm good!" />
       <button onClick={onClickButton}>Button</button>
     </>
   );
