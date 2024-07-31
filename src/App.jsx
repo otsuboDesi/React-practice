e30f90ea49eac565b6bdf5283b72e41014b68966
otsuboDesi <otsubodesignare@gmail.com>
2024-07-31 17:29:42 +0900

    Add: イベントの扱い方を確認

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
 export const App = () => {
+  const onClickButton = () => {
+    alert();
+  };
   return (
     <>
+      console.log("test")
+      {}
       <h1>Hello!</h1>
       <p>How are you?</p>
+      <button onClick={onClickButton}>Button</button>
     </>
   );
 };
