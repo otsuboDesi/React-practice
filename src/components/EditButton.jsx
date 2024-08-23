8270971c02c58d473f1eea78f8485595799ffa41
otsuboDesi <otsubodesignare@gmail.com>
2024-08-23 14:26:15 +0900

    Add: Stateを参照したいコンポーネントでuseContextを使う

--- src/components/EditButton.jsx
+++ src/components/EditButton.jsx
@@ -1,3 +1,6 @@
+import { useContext } from "react";
+import { AdminFlagContext } from "./providers/AdminFlagProvider";
+
 const style = {
   width: "100px",
   padding: "6px",
@@ -7,6 +10,10 @@ const style = {
 export const EditButton = (props) => {
   const { isAdmin } = props;
   // isAdminがfalseの時にボタンを非活性にする
+
+  // useContextの引数に参照するContextwお設定する
+  const contextValue = useContext(AdminFlagContext);
+  console.log(contextValue);
 
   return (
     <button style={style} disabled={!isAdmin}>
