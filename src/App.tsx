52cc346d708c65d8d96eb5309b72d9f55b1783e9
otsuboDesi <otsubodesignare@gmail.com>
2024-08-26 10:46:32 +0900

    Add: styled-componentsをインストール、inoutとbuttonを追加する

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 
 export const App = () => {
+  const [text, setText] = useState<string>("");
+  const [memos, setMemos] = useState<string[]>([]);
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setText(e.target.value);
+    console.log("text:", text);
+  };
+
+  const handleClickAdd = () => {
+    const newMemos = [...memos];
+    newMemos.push(text);
+    setMemos(newMemos);
+    console.log("memo:", memos);
+    setText("");
+  };
   return (
     <>
       <h1>Memo App</h1>
+      <input value={text} onChange={handleChange}></input>
+      <button onClick={handleClickAdd}>追加</button>
     </>
   );
 };
